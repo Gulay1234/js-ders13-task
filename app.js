804934087a38ce5d549289aea0fa65dd1a546514
Gulay <gulaynasibova28@gmail.com>
2024-12-20 21:16:32 +0400

    js-ders13-tasks
    js-ders13-tasks

---
+++ app.js
@@ -0,0 +1,34 @@
+// task1
+function round() {
+    let a = 3.7;
+    console.log(Math.round(a));
+}
+round()
+
+// task2
+    function min() {
+        let a = Math.min(3,7,2,9,10);
+        console.log(a);
+    }
+    min()
+    function max() {
+        let b = Math.max(4,6,1,3,9);
+        console.log(b);
+    }
+    max()
+
+
+    // task3
+    function random() {
+        console.log(Math.floor(Math.random()*50)+10) 
+    }
+    random()
+
+    // task4
+    function sqrt() {
+        let a = Math.sqrt(16);
+        let b = Math.sqrt(25);
+        let c = Math.sqrt(36);
+        console.log(a+b+c)
+    }
+    sqrt()
